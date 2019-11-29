055190598c60e329d456f0251d8ed1eafb9a957f
大木 <dkdk4646@gmail.com>
2019-11-29 19:29:54 +0900

    Lesson4課題、発展課題
    Testスクリプトに通常の課題、bossスクリプトに発展課題があります。

---
+++ Assets/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	void Start () {
+			int[] array = { 10, 20, 30, 40, 50 };
+			
+		for (int i = 0; i < array.Length; i++) {
+			Debug.Log (array [i]);	
+			}
+		for (int i = 4; i >= 0; i--) {
+			Debug.Log (array [i]);
+		}
+
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/boss.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss {
+	private int mp = 53;
+
+	// 魔法用の関数
+	public void Magic(){
+		this.mp -= 5;
+		if (this.mp > 5){
+			Debug.Log ("魔法攻撃をした。残りMPは○○。");
+		}else{
+			Debug.Log( "MPが足りないため魔法が使えない。" );
+		};			
+	}
+}
+
+public class boss : MonoBehaviour {
+	void Start () {
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss ();
+		// 魔法用の関数を呼び出す
+		for (int i = 1; i < 12; i++) {
+				lastboss.Magic();	
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
