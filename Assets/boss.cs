668302bae9555f5ffbd311f9fd07be0baddc9a44
大木 <dkdk4646@gmail.com>
2019-11-29 23:42:26 +0900

    Lesson4の課題の修正
    GameSceneを実行してもMagic関数が実行されていないことの修正。正しくmpの数字を出すようにしました。

--- Assets/boss.cs
+++ Assets/boss.cs
@@ -8,7 +8,7 @@ public class Boss {
 	public void Magic(){
 		this.mp -= 5;
 		if (this.mp > 5){
-			Debug.Log ("魔法攻撃をした。残りMPは○○。");
+			Debug.Log ("魔法攻撃をした。残りMPは"+ this.mp);
 		}else{
 			Debug.Log( "MPが足りないため魔法が使えない。" );
 		};			
